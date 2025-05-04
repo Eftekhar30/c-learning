7bb8a229252a38bbcd97e78eac5a94b2b8b54959
Eftekhar Hossain <eftekhar.hossain30@gmail.com>
2025-05-04 17:58:06 +0600

    initial commit

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
-int sum(int n);
-void primenum(int n){
-    
-}
+#include<math.h>
+#include<stdlib.h>
+int power();
+
 int main(){
-   
+    
+   printf("%d", power());
     return 0;
 }
 
+int power(){
+    int a,b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    int power = pow(a,b);
+    return power;
+}
